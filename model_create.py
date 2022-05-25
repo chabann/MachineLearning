c30fd69cce716ee3a9aee4ac92e5ea928b8e790a
a.jolobova <a.jolobova@spb.mcart.ru>
2022-05-26 01:39:48 +0300

    добавила больше комментариев

--- model_create.py
+++ model_create.py
@@ -32,36 +32,40 @@ def load_training_data(data_directory='datasets/aclImdb/train', split=0.8, limit
 
 def train_model(training_data, test_data, iterations=20):
     # Строим конвейер
-    nlp = spacy.load("en_core_web_sm")
-    if "textcat" not in nlp.pipe_names:
+    nlp = spacy.load("en_core_web_sm")  # Загружаем предобученную модель
+    if "textcat" not in nlp.pipe_names:   # Проверка есть ли textcat уже в конвейере
         textcat = nlp.create_pipe(
             "textcat", config={"architecture": "simple_cnn"}
         )
-        nlp.add_pipe(textcat, last=True)
+        nlp.add_pipe(textcat, last=True)  # добавление компонента textcat в конвейер
     else:
         textcat = nlp.get_pipe("textcat")
 
+    # Устанавливем метки классов
     textcat.add_label("pos")
     textcat.add_label("neg")
 
+    # Обучаем только textcat
     training_excluded_pipes = [
         pipe for pipe in nlp.pipe_names if pipe != "textcat"
     ]
-    with nlp.disable_pipes(training_excluded_pipes):
-        optimizer = nlp.begin_training()
+    with nlp.disable_pipes(training_excluded_pipes):  # Откючает все кроме textcat
+        optimizer = nlp.begin_training()  # Начальная функция оптимизатора
         # Итерация обучения
         print("Loss\t\tPrec.\tRec.\tF-score")
-        batch_sizes = compounding(4.0, 32.0, 1.001)
+        batch_sizes = compounding(4.0, 32.0, 1.001)  # Динамический размер батча (compounding)
 
         for i in range(iterations):
             loss = {}
             random.shuffle(training_data)
-            batches = minibatch(training_data, size=batch_sizes)
+            batches = minibatch(training_data, size=batch_sizes)  # Перемешиваем данные и создаем батчи
             for batch in batches:
                 text, labels = zip(*batch)
-                nlp.update(text, labels, drop=0.2, sgd=optimizer, losses=loss)
+                nlp.update(text, labels, drop=0.2, sgd=optimizer, losses=loss)  # Запускаем обучение
+                # Дропаут исключает часть данных для уменьшения переобучения
 
             with textcat.model.use_params(optimizer.averages):
+                # Оценка прогресса обучения
                 evaluation_results = evaluate_model(
                     tokenizer=nlp.tokenizer,
                     textcat=textcat,
